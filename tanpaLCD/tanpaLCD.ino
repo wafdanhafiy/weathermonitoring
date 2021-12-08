9836c8fb599efa839e641139830ceef2d18cdd35
wafdanhafiy <maswafdan@gmail.com>
2021-12-08 18:14:36 +0700

    init

--- tanpaLCD/tanpaLCD.ino
+++ tanpaLCD/tanpaLCD.ino
@@ -1,10 +1,10 @@
-#include <LiquidCrystal_I2C.h>
+//#include <LiquidCrystal_I2C.h>
 #define BLYNK_PRINT Serial
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 #include <DHT.h>
 
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+//LiquidCrystal_I2C lcd(0x27, 16, 2);
 DHT dht(D3, DHT11); //(sensor pin,sensor type)
 BlynkTimer timer;
 
@@ -31,37 +31,37 @@ void weather() {
   if (l == 0) {
     WidgetLED led1(V3);
     led1.on();
-    lcd.setCursor(9, 1);
-    lcd.print("L :");
-    lcd.print("High");
-    lcd.print(" ");
+//    lcd.setCursor(9, 1);
+//    lcd.print("L :");
+//    lcd.print("High");
+//    lcd.print(" ");
   } else if (l == 1) {
     WidgetLED led1(V3);
     led1.off();
-    lcd.setCursor(9, 1);
-    lcd.print("L :");
-    lcd.print("Low");
-    lcd.print(" ");
+//    lcd.setCursor(9, 1);
+//    lcd.print("L :");
+//    lcd.print("Low");
+//    lcd.print(" ");
   }
 
-  lcd.setCursor(0, 0);
-  lcd.print("T :");
-  lcd.print(t);
-
-  lcd.setCursor(0, 1);
-  lcd.print("H :");
-  lcd.print(h);
-
-  lcd.setCursor(9, 0);
-  lcd.print("R :");
-  lcd.print(r);
-  lcd.print("  ");
+//  lcd.setCursor(0, 0);
+//  lcd.print("T :");
+//  lcd.print(t);
+//
+//  lcd.setCursor(0, 1);
+//  lcd.print("H :");
+//  lcd.print(h);
+//
+//  lcd.setCursor(9, 0);
+//  lcd.print("R :");
+//  lcd.print(r);
+//  lcd.print("  ");
 }
 
 void setup() {
   Serial.begin(9600); // See the connection status in Serial Monitor
-  lcd.init();
-  lcd.backlight();
+//  lcd.init();
+//  lcd.backlight();
   Blynk.begin(auth, ssid, pass);
   dht.begin();
   // Setup a function to be called every second
