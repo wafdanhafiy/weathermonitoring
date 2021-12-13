ef5e36cc5b4e823d0036019a72725d96cf2ccc2e
wafdanhafiy <maswafdan@gmail.com>
2021-12-14 02:24:01 +0700

    fixing

--- blynkWeb/blynkWeb.ino
+++ blynkWeb/blynkWeb.ino
@@ -1,78 +1,55 @@
-/*************************************************************
+#define BLYNK_TEMPLATE_ID           "TMPLYL6H3s0Y"
+#define BLYNK_DEVICE_NAME           "Quickstart Device"
+#define BLYNK_AUTH_TOKEN            "zAdfBEB50NE9sw0Jtw_1dmWXIeasjxZJ"
 
-  This is a simple demo of sending and receiving some data.
-  Be sure to check out other examples!
- *************************************************************/
-
-// Template ID, Device Name and Auth Token are provided by the Blynk.Cloud
-// See the Device Info tab, or Template settings
-#define BLYNK_TEMPLATE_ID           "TMPLMNEAKzi-"
-#define BLYNK_DEVICE_NAME           "Weather Monitoring"
-#define BLYNK_AUTH_TOKEN            "AtOSv8LL0Vc6b00y3JBKM0iHupRg5bMk"
-
-
-// Comment this out to disable prints and save space
 #define BLYNK_PRINT Serial
-
-
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
+#include <DHT.h>
+
+DHT dht(D3, DHT11); //(sensor pin, sensor type)
+BlynkTimer timer;
 
 char auth[] = BLYNK_AUTH_TOKEN;
-
-// Your WiFi credentials.
-// Set password to "" for open networks.
 char ssid[] = "Mang haji_plus";
 char pass[] = "mutiahilman";
 
-BlynkTimer timer;
+void weather() {
+  int h = dht.readHumidity();
+  double t = dht.readTemperature();
+  int r = analogRead(A0);
+  int l = digitalRead(D4);
 
-// This function is called every time the Virtual Pin 0 state changes
-BLYNK_WRITE(V0)
-{
-  // Set incoming value from pin V0 to a variable
-  int value = param.asInt();
+  r = map(r, 0, 1023, 100, 0);
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
 
-  // Update state
-  Blynk.virtualWrite(V1, value);
+  Blynk.virtualWrite(V0, t);  // V0 untuk Temperature
+  Blynk.virtualWrite(V1, h);  // V1 untuk Humidity
+  Blynk.virtualWrite(V2, r);  // V2 untuk Rainfall
+
+  if (l == 0) {
+    WidgetLED led1(V3);
+    led1.on();
+  } else if (l == 1) {
+    WidgetLED led1(V3);
+    led1.off();
+  }
 }
 
-// This function is called every time the device is connected to the Blynk.Cloud
-BLYNK_CONNECTED()
-{
-  // Change Web Link Button message to "Congratulations!"
-  Blynk.setProperty(V3, "offImageUrl", "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations.png");
-  Blynk.setProperty(V3, "onImageUrl",  "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations_pressed.png");
-  Blynk.setProperty(V3, "url", "https://docs.blynk.io/en/getting-started/what-do-i-need-to-blynk/how-quickstart-device-was-made");
-}
-
-// This function sends Arduino's uptime every second to Virtual Pin 2.
-void myTimerEvent()
-{
-  // You can send any value at any time.
-  // Please don't send more that 10 values per second.
-  Blynk.virtualWrite(V2, millis() / 1000);
-}
-
-void setup()
-{
+void setup() {
   // Debug console
   Serial.begin(115200);
 
   Blynk.begin(auth, ssid, pass);
-  // You can also specify server:
-  //Blynk.begin(auth, ssid, pass, "blynk.cloud", 80);
-  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
-
+  dht.begin();
   // Setup a function to be called every second
-  timer.setInterval(1000L, myTimerEvent);
+  timer.setInterval(1000L, weather);
 }
 
-void loop()
-{
+void loop() {
   Blynk.run();
   timer.run();
-  // You can inject your own code or combine it with other sketches.
-  // Check other examples on how to communicate with Blynk. Remember
-  // to avoid delay() function!
 }
